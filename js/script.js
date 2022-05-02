2f1774d425c73dde32e44c9dee3f8ea46627503f
Levva01 <vellanifederico13@gmail.com>
2022-05-02 15:25:22 +0200

    progetto completato

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
+const container = document.querySelector(".container");
+
 for(let i = 1; i <= 100; i++){
 
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
-    } else if(i % 5 === 0){
-        console.log("Buzz");
-    } else if(i % 3 === 0){
-        console.log("Fizz");
-    } else {
-        console.log(i);
-    }
+    const element = document.createElement("div");
 
+    element.classList.add("box");
+
+    if(i % 3 === 0 && i % 5 === 0){
+        element.innerText = "FizzBuzz";
+        element.classList.add("bg-color-red");
+        container.append(element);
+
+    } else if(i % 5 === 0){
+        element.innerText = "Buzz";
+        element.classList.add("bg-color-yellow")
+        container.append(element);
+    } else if(i % 3 === 0){
+        element.innerText = "Fizz";
+        element.classList.add("bg-color-green")
+        container.append(element);
+    } else {
+        element.innerText = i;
+        element.classList.add("bg-color-blue")
+        container.append(element);
+    }
 }
